c282e08afac40148dd02fd50a03d0ef568d74a76
rafael cerdas <rafacerdas9@gmail.com>
2020-08-17 16:39:19 -0600

    Explicacion 1

---
+++ Explicacion.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rafa Cerdas on 8/17/20.
+//
+
+#include "Explicacion.h"
---
+++ Explicacion.h
@@ -0,0 +1,14 @@
+//
+// Created by Rafa Cerdas on 8/17/20.
+//
+
+#ifndef EXPLICACIONGIT_EXPLICACION_H
+#define EXPLICACIONGIT_EXPLICACION_H
+
+
+class Explicacion {
+//explicacion
+};
+
+
+#endif //EXPLICACIONGIT_EXPLICACION_H
